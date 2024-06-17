cf4a85908e592180cfd24b0c5e86b9666144a31b
reynaldo0 <reynaldooyusellino@gmail.com>
2024-06-17 15:18:30 +0700

    new : adding class explosion for grenades

--- shooter.py
+++ shooter.py
@@ -221,6 +221,20 @@ class Grenade(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for num in range (1,6):
+            img = pygame.image.load(f'assets/img/explosion/exp{num}.png').convert_alpha()
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            self.images.append(img)
+        self.frame_index = 0
+        self.image = self.images[self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.counter = 0
+
 # create sprite group
 bullet_group = pygame.sprite.Group()
 grenade_group = pygame.sprite.Group()
