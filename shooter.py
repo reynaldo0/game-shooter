4db384050ff94e819ac4e01f5abadb1f181c6a24
reynaldo0 <reynaldooyusellino@gmail.com>
2024-06-21 14:47:52 +0700

    new : speed background scrolling

--- shooter.py
+++ shooter.py
@@ -77,10 +77,10 @@ def draw_bg():
     screen.fill(BG)
     width = sky_img.get_width()
     for x in range(5):
-        screen.blit(sky_img, ((x * width) - bg_scroll , 0))  
-        screen.blit(mountain_img, ((x * width) - bg_scroll, SCREEN_HEIGHT - mountain_img.get_height() - 300))  
-        screen.blit(pine1_img, ((x * width) - bg_scroll, SCREEN_HEIGHT - pine1_img.get_height() - 150))  
-        screen.blit(pine2_img, ((x * width)  - bg_scroll, SCREEN_HEIGHT - pine2_img.get_height()))  
+        screen.blit(sky_img, ((x * width) - bg_scroll * 0.5 , 0))  
+        screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))  
+        screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))  
+        screen.blit(pine2_img, ((x * width)  - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))  
 
 class soldier(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed, ammo, grenades):
